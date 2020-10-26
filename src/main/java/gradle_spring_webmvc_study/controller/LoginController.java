80ddce1795f919a64a3edecf19f6595df982547c
teacher <net94.teacher@gmail.com>
2020-10-26 23:21:11 +0900

    임포트 정리

--- src/main/java/gradle_spring_webmvc_study/controller/LoginController.java
+++ src/main/java/gradle_spring_webmvc_study/controller/LoginController.java
@@ -7,7 +7,6 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import gradle_spring_webmvc_study.dto.JobCode;
